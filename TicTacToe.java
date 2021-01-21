0341500c453dcfbb19aa7ccd946a4961b4af0930
ravindersingh8a <ravindersingh8a@gmail.com>
2021-01-21 20:49:13 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,149 +5,11 @@ import java.util.Scanner;
 public class TicTacToe
 {
 
-
     int board[]= {1,2,3,4,5,6,7,8,9};
-    char Nboard[]={'\0','\0','\0','\0','\0','\0','\0','\0','\0'};
+    char InitBoard[]={' ',' ',' ',' ',' ',' ',' ',' ',' '};
     int magicSquare[]={2,7,6,9,5,1,4,3,8};
     ArrayList<Integer> user;
     ArrayList<Integer> machine;
-
-    public TicTacToe(){
-        user = new ArrayList<>();
-        machine = new ArrayList<>();
-    }
-
-
-    boolean userTurn(int pos){
-        if(board[pos-1]!=10 && board[pos-1]!=11){
-            user.add(magicSquare[pos-1]);
-            board[pos-1]=10;
-            Nboard[pos-1]='O';
-            return true;
-        }
-        return false;
-    }
-
-    void machineTurn(){
-        boolean flag = false;
-        if(machine.size()==0){
-            board[(user.get(0)+1)%9]=11;
-            Nboard[(user.get(0)+1)%9]='X';
-            machine.add(magicSquare[(user.get(0)+1)%9]);
-            flag=true;
-            return;
-        }
-        if(!flag && machine.size()==1){
-            for(int i=0; i<user.size()-1; i++){
-                for(int j=i+1; j<user.size(); j++){
-                    int temp = 15 - (user.get(i)+user.get(j));
-                    int k=0;
-                    for(k=0; k<this.magicSquare.length; k++){
-                        if(magicSquare[k]==temp)
-                            break;
-                    }
-                    System.out.println(k);
-                    if(temp>=1 && temp<= 9 && board[k]!=10 && board[k]!=11){
-                        board[k]=11;
-                        Nboard[k]='X';
-                        machine.add(temp);
-                        flag=true;
-                        return;
-                    }
-                }
-            }
-        }
-        if(!flag && machine.size()>1){
-            for(int i=0; i<user.size()-1; i++){
-                for(int j=i+1; j<user.size(); j++){
-                    int temp = 15 - (user.get(i)+user.get(j));
-                    int k=0;
-                    for(k=0; k<magicSquare.length; k++){
-                        if(magicSquare[k]==temp)
-                            break;
-                    }
-                    if(temp>=1 && temp<= 9 && board[k]!=10 && board[k]!=11){
-                        board[k]=11;
-                        Nboard[k]='X';
-                        machine.add(temp);
-                        flag=true;
-                        return;
-                    }
-                }
-            }
-            for(int i=0; i<machine.size()-1; i++){
-                for(int j=i+1; j<machine.size(); j++){
-                    int temp = 15-(machine.get(i)+machine.get(j));
-                    int k=0;
-                    for(k=0; k<magicSquare.length; k++){
-                        if(magicSquare[k]==temp)
-                            break;
-                    }
-                    if(temp>=1 && temp<= 9 && board[k]!=10 && board[k]!=11){
-                        board[k]=11;
-                        Nboard[k]='X';
-                        machine.add(temp);
-                        flag=true;
-                        return;
-                    }
-                }
-            }
-
-        }
-
-        if(!flag){
-            for(int i=0; i<board.length; i++){
-                if(board[i]!=0 || board[i]!=1){
-                    board[i]=11;
-                    board[i]='X';
-                    machine.add(magicSquare[i]);
-                    break;
-                }
-            }
-        }
-
-
-    }
-
-
-
-    boolean checkUserWin(){
-        if((board[0]==10 && board[1]==10 && board[2]==10) || (board[3]==10 && board[4]==10 && board[5]==10) || (board[6]==10 && board[7]==10 && board[8]==10))
-            return true;
-        if((board[0]==10 && board[3]==10 && board[6]==10) || (board[3]==10 && board[4]==10 && board[7]==10) || (board[2]==10 && board[5]==10 && board[8]==10))
-            return true;
-        if((board[0]==10 && board[4]==10 && board[8]==10) || (board[2]==10 && board[4]==10 && board[6]==10))
-            return true;
-        return false;
-    }
-
-    boolean checkMachineWin(){
-        if((board[0]==11 && board[1]==11 && board[2]==11) || (board[3]==11 && board[4]==11 && board[5]==11) || (board[6]==11 && board[7]==11 && board[8]==11))
-            return true;
-        if((board[0]==11 && board[3]==11 && board[6]==11) || (board[3]==11 && board[4]==11 && board[7]==11) || (board[2]==11 && board[5]==11 && board[8]==11))
-            return true;
-        if((board[0]==11 && board[4]==11 && board[8]==11) || (board[2]==11 && board[4]==11 && board[6]==11))
-            return true;
-        return false;
-    }
-
-
-    void displayBoard(){
-        System.out.println("  "+Nboard[0]+" | "+Nboard[1]+" | "+Nboard[2]);
-        System.out.println("------------");
-        System.out.println("  "+Nboard[3]+" | "+Nboard[4]+" | "+Nboard[5]);
-        System.out.println("------------");
-        System.out.println("  "+Nboard[6]+" | "+Nboard[7]+" | "+Nboard[8]);
-    }
-    void displayBoard2(){
-        System.out.println("  "+board[0]+" | "+board[1]+" | "+board[2]);
-        System.out.println("------------");
-        System.out.println("  "+board[3]+" | "+board[4]+" | "+board[5]);
-        System.out.println("------------");
-        System.out.println("  "+board[6]+" | "+board[7]+" | "+board[8]);
-    }
-
-
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
@@ -201,8 +63,146 @@ public class TicTacToe
             }
         }
         if(!flag){
-            System.out.println("There is a Tie!");
+            System.out.println("Game is Tie!");
         }
     }
 
+
+    //Here 0 is User and X is Machine
+    public TicTacToe(){
+        user = new ArrayList<>();
+        machine = new ArrayList<>();
+    }
+
+
+    boolean userTurn(int pos){
+        if(board[pos-1]!=10 && board[pos-1]!=11){
+            user.add(magicSquare[pos-1]);
+            board[pos-1]=10;
+            InitBoard[pos-1]='O';
+            return true;
+        }
+        return false;
+    }
+
+    void machineTurn(){
+        boolean flag = false;
+        if(machine.size()==0){
+            board[(user.get(0)+1)%9]=11;
+            InitBoard[(user.get(0)+1)%9]='X';
+            machine.add(magicSquare[(user.get(0)+1)%9]);
+            flag=true;
+            return;
+        }
+        if(!flag && machine.size()==1){
+            for(int i=0; i<user.size()-1; i++){
+                for(int j=i+1; j<user.size(); j++){
+                    int temp = 15 - (user.get(i)+user.get(j));
+                    int k=0;
+                    for(k=0; k<this.magicSquare.length; k++){
+                        if(magicSquare[k]==temp)
+                            break;
+                    }
+                    System.out.println(k);
+                    if(temp>=1 && temp<= 9 && board[k]!=10 && board[k]!=11){
+                        board[k]=11;
+                        InitBoard[k]='X';
+                        machine.add(temp);
+                        flag=true;
+                        return;
+                    }
+                }
+            }
+        }
+        if(!flag && machine.size()>1){
+            for(int i=0; i<user.size()-1; i++){
+                for(int j=i+1; j<user.size(); j++){
+                    int temp = 15 - (user.get(i)+user.get(j));
+                    int k=0;
+                    for(k=0; k<magicSquare.length; k++){
+                        if(magicSquare[k]==temp)
+                            break;
+                    }
+                    if(temp>=1 && temp<= 9 && board[k]!=10 && board[k]!=11){
+                        board[k]=11;
+                        InitBoard[k]='X';
+                        machine.add(temp);
+                        flag=true;
+                        return;
+                    }
+                }
+            }
+            for(int i=0; i<machine.size()-1; i++){
+                for(int j=i+1; j<machine.size(); j++){
+                    int temp = 15-(machine.get(i)+machine.get(j));
+                    int k=0;
+                    for(k=0; k<magicSquare.length; k++){
+                        if(magicSquare[k]==temp)
+                            break;
+                    }
+                    if(temp>=1 && temp<= 9 && board[k]!=10 && board[k]!=11){
+                        board[k]=11;
+                        InitBoard[k]='X';
+                        machine.add(temp);
+                        flag=true;
+                        return;
+                    }
+                }
+            }
+
+        }
+
+        if(!flag){
+            for(int i=0; i<board.length; i++){
+                if(board[i]!=0 || board[i]!=1){
+                    board[i]=11;
+                    board[i]='X';
+                    machine.add(magicSquare[i]);
+                    break;
+                }
+            }
+        }
+
+
+    }
+
+
+
+    boolean checkUserWin(){
+        if((board[0]==10 && board[1]==10 && board[2]==10) || (board[3]==10 && board[4]==10 && board[5]==10) || (board[6]==10 && board[7]==10 && board[8]==10))
+            return true;
+        if((board[0]==10 && board[3]==10 && board[6]==10) || (board[3]==10 && board[4]==10 && board[7]==10) || (board[2]==10 && board[5]==10 && board[8]==10))
+            return true;
+        if((board[0]==10 && board[4]==10 && board[8]==10) || (board[2]==10 && board[4]==10 && board[6]==10))
+            return true;
+        return false;
+    }
+
+    boolean checkMachineWin(){
+        if((board[0]==11 && board[1]==11 && board[2]==11) || (board[3]==11 && board[4]==11 && board[5]==11) || (board[6]==11 && board[7]==11 && board[8]==11))
+            return true;
+        if((board[0]==11 && board[3]==11 && board[6]==11) || (board[3]==11 && board[4]==11 && board[7]==11) || (board[2]==11 && board[5]==11 && board[8]==11))
+            return true;
+        if((board[0]==11 && board[4]==11 && board[8]==11) || (board[2]==11 && board[4]==11 && board[6]==11))
+            return true;
+        return false;
+    }
+
+
+    void displayBoard(){
+        System.out.println("  "+InitBoard[0]+" | "+InitBoard[1]+" | "+InitBoard[2]);
+        System.out.println("------------");
+        System.out.println("  "+InitBoard[3]+" | "+InitBoard[4]+" | "+InitBoard[5]);
+        System.out.println("------------");
+        System.out.println("  "+InitBoard[6]+" | "+InitBoard[7]+" | "+InitBoard[8]);
+    }
+    void displayBoard2(){
+        System.out.println("  "+board[0]+" | "+board[1]+" | "+board[2]);
+        System.out.println("------------");
+        System.out.println("  "+board[3]+" | "+board[4]+" | "+board[5]);
+        System.out.println("------------");
+        System.out.println("  "+board[6]+" | "+board[7]+" | "+board[8]);
+    }
+
 }
+
